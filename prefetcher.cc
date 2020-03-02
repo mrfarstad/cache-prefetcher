38b7dd17783c581b1cf7a6095c4b3a956177874a
Martin Farstad <farstad2k@gmail.com>
2020-03-02 09:12:23 +0100

    New implementation without pointers to entries

--- prefetcher.cc
+++ prefetcher.cc
@@ -6,6 +6,7 @@
 
 #include "interface.hh"
 #include "stdlib.h"
+#include "stdio.h"
 
 /*
  * Thoughts:
@@ -32,15 +33,19 @@
 
 struct ghb_entry {
   Addr address;
-  ghb_entry* prev_occurence;
-  ghb_entry* next_occurence;
+  bool valid;
+  // Current index
   int index;
+  // Previous occurence index
+  int prev;
+  // Next occurence index
+  int next;
 };
 
-ghb_entry* ghb_add_entry(Addr address);
-ghb_entry* ghb_get_prev_occurence(Addr address);
+void ghb_add_entry(Addr address);
+int ghb_get_prev_occurence(Addr address);
 
-ghb_entry** ghb = NULL;
+ghb_entry* ghb = NULL;
 int ghb_head = -1;
 int depth = 0;
 
@@ -49,70 +54,58 @@ void prefetch_init(void) {
     /* This is the place to initialize data structures. */
 
     //DPRINTF(HWPrefetch, "Initialized sequential-on-access prefetcher\n");
-    ghb = (ghb_entry**) malloc(sizeof(ghb_entry*) * GHB_SIZE);
-    for (unsigned int i = 0; i < GHB_SIZE; i++) {
-      ghb[i] = NULL;
+    ghb = (ghb_entry*) malloc(sizeof(ghb_entry) * GHB_SIZE);
+    for (int i = 0; i < GHB_SIZE; i++) {
+      ghb[i].valid = false;
+      ghb[i].index = i;
     }
 }
 
-void prefetch_access(AccessStat stat) {
- // Addr pf_addr = stat.mem_addr + BLOCK_SIZE;
- // ////
- // /////*
- // //// * Issue a prefetch request if a demand miss occured,
- // //// * and the block is not already in cache.
- // //// */
- // if (stat.miss && !in_cache(pf_addr)) {
- //     issue_prefetch(pf_addr);
- // }
-  ghb_entry* entry = ghb_add_entry(stat.mem_addr)->prev_occurence;
-  while (entry != NULL && depth < GHB_DEPTH) {
-    Addr pf_addr = ghb[(entry->index + 1) % GHB_SIZE]->address;
-    //if (stat.miss && !in_cache(pf_addr)) {
-    issue_prefetch(pf_addr);
-    //}
-    entry = entry->prev_occurence;
-    depth++;
-  }
-  depth = 0;
-}
-
-ghb_entry* ghb_get_prev_occurence(Addr address) {
-  if (ghb_head == -1) return NULL;
-  size_t index = (ghb_head - 1 + GHB_SIZE) % GHB_SIZE;
+int ghb_get_prev_occurence(Addr address) {
+  int index = (ghb_head - 1 + GHB_SIZE) % GHB_SIZE;
   while (index != ghb_head) {
-    ghb_entry* entry = ghb[index];
-    if (entry == NULL) return NULL;
-    else if (entry->address == address) return entry;
+    if (!ghb[index].valid) return -1;
+    else if (ghb[index].address == address) return index;
     index = (index - 1 + GHB_SIZE) % GHB_SIZE;
   }
-  return NULL;
+  return -1;
 }
 
-ghb_entry* ghb_add_entry(Addr address) {
-  // Increment ghb
+void ghb_add_entry(Addr address) {
+  // Increment head pointer
   ghb_head = (ghb_head + 1) % GHB_SIZE;
 
   // Handle existing entry
-  ghb_entry* old = ghb[ghb_head];
-  if (old != NULL) {
-    // Reset next_occurence's pointer to this struct
-    if (old->next_occurence != NULL) old->next_occurence->prev_occurence = NULL;
-    free(old);
-    ghb[ghb_head] = NULL;
+  ghb_entry* entry = &ghb[ghb_head];
+  if (entry->valid && entry->next != -1) {
+    ghb[entry->next].prev = -1;
   }
 
-  ghb_entry* entry = (ghb_entry *) malloc(sizeof(ghb_entry));
+  // Add new entry
   entry->address = address;
-  entry->index = ghb_head;
-  entry->next_occurence = NULL;
-  entry->prev_occurence = ghb_get_prev_occurence(address);
-  if (entry->prev_occurence != NULL) entry->prev_occurence->next_occurence = entry;
+  entry->valid = true;
+  entry->next = -1;
 
-  // Add entry
-  ghb[ghb_head] = entry;
+  // Find previous occurence
+  entry->prev = ghb_get_prev_occurence(address);
 
-  return entry;
+  // Handle previous occurrence
+  if (entry->prev != -1) {
+    ghb[entry->prev].next = ghb_head;
+  }
+}
+
+void prefetch_access(AccessStat stat) {
+  ghb_add_entry(stat.mem_addr);
+  int prev = ghb[ghb_head].prev;
+  int next;
+  while (prev != -1 && depth < GHB_DEPTH) {
+    next = (prev + 1) % GHB_SIZE;
+    if (!in_cache(ghb[next].address)) issue_prefetch(ghb[next].address);
+    prev = ghb[prev].prev;
+    depth++;
+  }
+  depth = 0;
 }
 
 void prefetch_complete(Addr addr) {
