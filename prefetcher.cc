7f71a1e411e8d6cf94201ad2becc026c82aef01c
Martin Farstad <farstad2k@gmail.com>
2020-03-03 13:28:08 +0100

    Improve types and remove debug code

--- prefetcher.cc
+++ prefetcher.cc
@@ -30,44 +30,44 @@
  * PREFETCHER
  * */
 
-#define GHB_SIZE 4096
-#define AIT_SIZE 2048
+#define GHB_SIZE 4096 // uint16_t
+#define AIT_SIZE 2048 // uint16_t
 // This size cannot be dynamic, because hardware=)
-#define MARKOV_SIZE 20
+#define MARKOV_SIZE 20 // uint8_t
 
 struct ghb_entry {
   Addr address;
   bool valid;
   // Previous occurence index
-  int64_t prev;
+  int16_t prev;
   // Next occurence index
-  int64_t next;
+  int16_t next;
 };
 
 struct ait_entry {
   Addr address;
   bool valid;
   // Index of last GHB entry
-  int64_t entry;
+  int16_t entry;
 };
 
 struct markov_entry {
   Addr address;
   bool valid;
-  int64_t count;
+  int8_t count;
 };
 
 void ghb_add_entry(Addr address);
-int64_t ghb_get_prev_occurence(Addr address);
+int16_t ghb_get_prev_occurence(Addr address);
 
 ghb_entry* ghb = NULL;
-int64_t ghb_head = -1;
+int16_t ghb_head = -1;
 
 ait_entry* ait = NULL;
-int64_t ait_head = -1;
+uint16_t ait_head = 0;
 
 markov_entry* markov = NULL;
-uint64_t markov_head = 0;
+uint8_t markov_head = 0;
 
 void prefetch_init(void) {
     /* Called before any calls to prefetch_access. */
@@ -76,19 +76,19 @@ void prefetch_init(void) {
     //DPRINTF(HWPrefetch, "Initialized sequential-on-access prefetcher\n");
     ghb = (ghb_entry*) malloc(sizeof(ghb_entry) * GHB_SIZE);
     ait = (ait_entry*) malloc(sizeof(ait_entry) * AIT_SIZE);
-    for (int64_t i = 0; i < GHB_SIZE; i++) {
+    for (uint16_t i = 0; i < GHB_SIZE; i++) {
       ghb[i].valid = false;
     }
-    for (int64_t i = 0; i < AIT_SIZE; i++) {
+    for (uint16_t i = 0; i < AIT_SIZE; i++) {
       ait[i].valid = false;
     }
     markov = (markov_entry *) malloc(sizeof(markov_entry) * MARKOV_SIZE);
 }
 
-int64_t ghb_get_prev_occurence(Addr address) {
+int16_t ghb_get_prev_occurence(Addr address) {
   // Search for previous entry
-  int64_t entry;
-  for (int64_t i = 0; i < AIT_SIZE; i++) {
+  int16_t entry;
+  for (uint16_t i = 0; i < AIT_SIZE; i++) {
     if (!ait[i].valid) break;
     else if (ait[i].address == address) {
       // Handle found entry
@@ -100,18 +100,11 @@ int64_t ghb_get_prev_occurence(Addr address) {
   // If entry not found, add it to the table
   
   // Increment head pointer
-  ait_head = (ait_head + 1) % AIT_SIZE;
   ait[ait_head].address = address;
   ait[ait_head].entry = ghb_head;
   ait[ait_head].valid = true;
+  ait_head = (ait_head + 1) % AIT_SIZE;
   return -1;
-  //int64_t index = (ghb_head - 1 + GHB_SIZE) % GHB_SIZE;
-  //while (index != ghb_head) {
-  //  if (!ghb[index].valid) return -1;
-  //  else if (ghb[index].address == address) return index;
-  //  index = (index - 1 + GHB_SIZE) % GHB_SIZE;
-  //}
-  //return -1;
 }
 
 void ghb_add_entry(Addr address) {
@@ -138,12 +131,9 @@ void ghb_add_entry(Addr address) {
   }
 }
 
-int count = 0;
-
-
 void prefetch_access(AccessStat stat) {
   ghb_add_entry(stat.mem_addr);
-  int64_t prev = ghb[ghb_head].prev;
+  int16_t prev = ghb[ghb_head].prev;
 
   Addr candidate = 0;
   if (prev == -1 ) {
@@ -152,30 +142,10 @@ void prefetch_access(AccessStat stat) {
     return;
   }
 
-  int64_t next;
-
-  // TODO: Remome std::map, because we don't know how to handle size issues
-  //std::map<Addr, int64_t> m;
-  //while (prev != -1) {
-  //  next = (prev + 1) % GHB_SIZE;
-  //  m[ghb[next].address]++;
-  //  prev = ghb[prev].prev;
-  //}
-
-  ////Addr candidate = 0;
-  //int64_t current_max = 0;
-  //std::map<Addr, int64_t>::iterator it;
-  //for(it = m.begin(); it != m.end(); ++it ) {
-  //    if (it->second > current_max) {
-  //        candidate = it->first;
-  //        current_max = it->second;
-  //    }
-  //  printf("%lu (%ld) ", it->first, it->second);
-  //}
-  //printf("=> %lu\n", candidate);
+  int16_t next;
 
   // Initialize the data structures
-  for (int64_t i = 0; i < MARKOV_SIZE; i++) {
+  for (uint8_t i = 0; i < MARKOV_SIZE; i++) {
     markov[i].valid = false;
   }
   // Loop throught the GHB linked list of previous occurences
@@ -183,14 +153,14 @@ void prefetch_access(AccessStat stat) {
     next = (prev + 1) % GHB_SIZE;
     // Increment markov entry if exists
     bool exists = false;
-    for (uint64_t i = 0; i < MARKOV_SIZE; i++) {
+    for (uint8_t i = 0; i < MARKOV_SIZE; i++) {
       if (markov[i].valid && markov[i].address == ghb[next].address) {
         markov[i].count++;
         exists = true;
         break;
       } 
     }
-  //  // If markov entry not exists, create entry
+    // If markov entry not exists, create entry
     if (!exists && markov_head < MARKOV_SIZE) {
       markov[markov_head].address = ghb[next].address;
       markov[markov_head].count = 1;
@@ -199,8 +169,8 @@ void prefetch_access(AccessStat stat) {
     prev = ghb[prev].prev;
   }
 
-  uint64_t count = 0;
-  for (uint64_t i = 0; i < MARKOV_SIZE; i++) {
+  uint8_t count = 0;
+  for (uint8_t i = 0; i < MARKOV_SIZE; i++) {
     if (!markov[i].valid) break;
     else if (markov[i].count > count) {
       candidate = markov[i].address;
@@ -208,12 +178,6 @@ void prefetch_access(AccessStat stat) {
     }
   }
   markov_head = 0;
-
-  for (uint64_t i = 0; i < MARKOV_SIZE; i++) {
-    if (!markov[i].valid) break;
-    printf("%lu (%ld) ", markov[i].address, markov[i].count);
-  }
-  printf("=> %lu\n", candidate);
   
   if (!in_cache(candidate) && !in_mshr_queue(candidate)) {
     issue_prefetch(candidate);
