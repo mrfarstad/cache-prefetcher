640fd6647b86aa9ec014e8e0525f380498c9b519
Martin Farstad <farstad2k@gmail.com>
2020-03-20 13:25:32 +0100

    Add hybrid adjacent prefetcher when no prev found

--- prefetcher.cc
+++ prefetcher.cc
@@ -17,6 +17,7 @@
 #define AIT_SIZE 512
 #define DEPTH 3
 #define WIDTH 3
+#define STRIDED 4
 
 
 void ghb_add_entry(Addr addr);
@@ -54,44 +55,7 @@ void prefetch_init(void) {
   for (int16_t i = 0; i < AIT_SIZE; i++) ait[i].valid = false;
 }
 
-//int16_t ghb_get_prev_entry(Addr addr) {
-//  int16_t prev = -1;
-//  int16_t index = (ghb_head + 1) % GHB_SIZE;
-//  while (index != ghb_head) {
-//    if (!ghb[index].valid) return -1;
-//    else if (ghb[index].addr == addr) return index;
-//    index = (index + 1) % GHB_SIZE;
-//  }
-//  return prev;
-//}
-
 int16_t ait_get_prev_ghb_entry(Addr delta, bool sign) {
-  // TODO: Implement hash function
-  // Search for previous entry
-  //int16_t entry;
-  //for (int16_t i = 0; i < AIT_SIZE; i++) {
-  //  if (!ait[i].valid) break;
-  //  else if (ait[i].addr == addr) {
-  //    // Handle found entry
-  //    entry = ait[i].entry;
-  //    if (!ghb[entry].valid) entry = -1;
-  //    ait[i].entry = ghb_head;
-  //    return entry;
-  //  }
-  //}
-
-  ////// If entry not found in ait, search in ghb. TODO: Remove?
-  ////int16_t prev = ghb_get_prev_entry(addr);
-
-  //// Increment head pointer
-  //ait[ait_head].addr = addr;
-  //ait[ait_head].entry = ghb_head;
-  //ait[ait_head].valid = true;
-  //ait_head = (ait_head + 1) % AIT_SIZE;
-  //return -1;
-  //return prev;
-
-  // TODO: Implement later
   int16_t hash = delta % AIT_SIZE;
   ait_entry* bucket = &ait[hash];
   int16_t b_delta = bucket->delta;
@@ -158,29 +122,28 @@ void prefetch_access(AccessStat stat) {
   Addr addr = stat.mem_addr;
   bool prefetched = get_prefetch_bit(addr);
   if (stat.miss || prefetched) {
-    //ghb_add_entry(addr);
-    //for (int16_t i = 0; i < GHB_SIZE; i++) {
-    //  if (i == ghb_head) continue;
-    //  else if (ghb[i].addr == addr) {
-    //    printf("found!\n");
-    //  }
-    //}
-    // TODO: Add entry to GHB
     ghb_add_entry(addr);
     if (prefetched) {
      clear_prefetch_bit(addr);
      return;
     }
-    // TODO: Handle prefetch candidates
     int16_t prev = ghb[ghb_head].prev;
-    //printf("prev: %d\n", prev);
-    int16_t depth = 0;
-    int16_t width = 0;
+    Addr tmp_addr = addr + BLOCK_SIZE;
+    if (prev == -1) {
+      uint8_t strided = 0;
+      while (strided++ < STRIDED) {
+        if (!in_cache(tmp_addr) && !in_mshr_queue(tmp_addr))
+          issue_prefetch(tmp_addr);
+        tmp_addr += BLOCK_SIZE;
+      }
+    }
+    uint8_t depth = 0;
+    uint8_t width = 0;
     Addr prev_addr;
     Addr cand_addr;
     Addr delta;
     bool sign;
-    while (prev != -1 && depth < DEPTH && width < WIDTH) {
+    while (prev != -1 && depth < DEPTH && width++ < WIDTH) {
       prev_addr = ghb[prev].addr;
       cand_addr = ghb[(prev + 1) % GHB_SIZE].addr;
       if (prev_addr > cand_addr) {
@@ -191,7 +154,6 @@ void prefetch_access(AccessStat stat) {
         sign = 1;
       }
       prev = ghb[prev].prev;
-      width++;
       if (sign && addr + delta < MAX_PHYS_MEM_ADDR)
         addr += delta;
       else if (!sign && addr > delta)
@@ -210,4 +172,5 @@ void prefetch_access(AccessStat stat) {
 void prefetch_complete(Addr addr) {
   set_prefetch_bit(addr);
 }
+
 
