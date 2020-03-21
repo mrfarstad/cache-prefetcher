1c1bd7cd6c17b394d9fa1b3e1e11dc6394652edf
Martin Farstad <farstad2k@gmail.com>
2020-03-21 17:32:16 +0100

    Initial PC/DC implementation, currently bad performance

--- prefetcher.cc
+++ prefetcher.cc
@@ -7,22 +7,26 @@
 #include "interface.hh"
 #include "stdlib.h"
 #include "stdio.h"
-#include <queue>
 
 /*
  * PREFETCHER
  * */
 
 #define GHB_SIZE 1024
-#define AIT_SIZE 512
+#define AIT_SIZE 1024
 #define DEPTH 3
 #define WIDTH 3
-#define STRIDED 5
+#define STRIDED 4
 
+struct delta_t {
+  Addr delta;
+  bool sign;
+};
 
-void ghb_add_entry(Addr addr);
-int16_t ait_get_prev_ghb_entry(Addr addr, bool sign);
-int16_t ghb_get_prev_entry(Addr addr);
+struct deltas_t {
+  delta_t d1;
+  delta_t d2;
+};
 
 struct ghb_entry {
   Addr addr;
@@ -32,11 +36,14 @@ struct ghb_entry {
 };
 
 struct ait_entry {
-  Addr delta;
-  bool sign;
+  deltas_t deltas;
   bool valid;
   int16_t entry;
 };
+
+void ghb_add_entry(Addr addr);
+int16_t ait_get_prev_ghb_entry(deltas_t d);
+int16_t ghb_get_prev_entry(Addr addr);
 
 ghb_entry* ghb = NULL;
 int16_t ghb_head = -1;
@@ -55,26 +62,55 @@ void prefetch_init(void) {
   for (int16_t i = 0; i < AIT_SIZE; i++) ait[i].valid = false;
 }
 
-int16_t ait_get_prev_ghb_entry(Addr delta, bool sign) {
-  // TODO: Implement later
-  int16_t hash = delta % AIT_SIZE;
-  ait_entry* bucket = &ait[hash];
-  int16_t b_delta = bucket->delta;
-  int16_t b_entry = bucket->entry;
-  bool b_valid = bucket->valid;
-  bool b_sign = bucket->sign;
-  bucket->delta = delta;
-  bucket->sign = sign;
+int16_t ait_get_prev_ghb_entry(deltas_t d) {
+  int16_t entry;
+  ait_entry* bucket;
+  for (int16_t i = 0; i < AIT_SIZE; i++) {
+    bucket = &ait[i];
+    if (!bucket->valid) break;
+    else if (
+        bucket->deltas.d1.delta == d.d1.delta &&
+        bucket->deltas.d1.sign == d.d1.sign &&
+        bucket->deltas.d2.delta == d.d2.delta &&
+        bucket->deltas.d2.sign == d.d2.sign
+    ) {
+      // Handle found entry
+      entry = ait[i].entry;
+      if (!ghb[bucket->entry].valid) entry = -1;
+      ait[i].entry = ghb_head;
+      return entry;
+    }
+  }
+
+  bucket = &ait[ait_head];
+  bucket->deltas = d;
   bucket->entry = ghb_head;
   bucket->valid = true;
-  // Not ait init && not overwritten ghb entry && equal deltas and signes
-  if (b_valid && ghb[b_entry].valid && b_delta == delta && b_sign == sign)
-    return b_entry;
+  ait_head = (ait_head + 1) % AIT_SIZE;
   return -1;
+
+  // TODO: Find a better hash function
+  // Use std:hash or something
+  //int16_t hash = (d.d1.delta ^ d.d2.delta) % AIT_SIZE;
+  //int16_t hash = (d.d1.delta + d.d2.delta) % AIT_SIZE;
+  //ait_entry* bucket = &ait[hash];
+  //deltas_t b_deltas = bucket->deltas;
+  //int16_t b_entry = bucket->entry;
+  //bool b_valid = bucket->valid;
+  //bucket->deltas = d;
+  //bucket->entry = ghb_head;
+  //bucket->valid = true;
+  //// Not ait init && not overwritten ghb entry && equal deltas and signes
+  //if (b_valid && ghb[b_entry].valid &&
+  //    b_deltas.d1.delta == d.d1.delta && b_deltas.d1.sign == d.d1.sign &&
+  //    b_deltas.d2.delta == d.d2.delta && b_deltas.d2.sign == d.d2.sign)
+  //  return b_entry;
+  //return -1;
 }
 
 
 void ghb_add_entry(Addr addr) {
+  int16_t prev_head = (ghb_head - 1 + GHB_SIZE) % GHB_SIZE;
   int16_t tmp_head = ghb_head;
   ghb_head = (ghb_head + 1) % GHB_SIZE;
 
@@ -88,18 +124,28 @@ void ghb_add_entry(Addr addr) {
 
   // Find prev entry
   int16_t prev;
-  if (tmp_head != -1) {
-    Addr delta;
-    Addr tmp_addr = ghb[tmp_head].addr;
-    bool sign;
+  delta_t d1;
+  delta_t d2;
+  if (tmp_head != -1 && ghb[prev_head].valid) {
+    Addr tmp_addr = ghb[prev_head].addr;
     if (tmp_addr > addr) {
-      delta = tmp_addr - addr;
-      sign = 0;
+      d1.delta = tmp_addr - addr;
+      d1.sign = 0;
     } else {
-      delta = addr - tmp_addr;
-      sign = 1;
+      d1.delta = addr - tmp_addr;
+      d1.sign = 1;
     }
-    prev = ait_get_prev_ghb_entry(delta, sign);
+
+    tmp_addr = ghb[tmp_head].addr;
+    if (tmp_addr > addr) {
+      d2.delta = tmp_addr - addr;
+      d2.sign = 0;
+    } else {
+      d2.delta = addr - tmp_addr;
+      d2.sign = 1;
+    }
+
+    prev = ait_get_prev_ghb_entry((struct deltas_t) { d1, d2 });
     if (prev == ghb_head) fprintf(stderr, "error: prev == ghb_head");
   } else {
     prev = -1;
@@ -129,15 +175,15 @@ void prefetch_access(AccessStat stat) {
      return;
     }
     int16_t prev = ghb[ghb_head].prev;
-    Addr tmp_addr = addr + BLOCK_SIZE;
-    if (prev == -1) {
-      uint8_t strided = 0;
-      while (strided++ < STRIDED) {
-        if (!in_cache(tmp_addr) && !in_mshr_queue(tmp_addr))
-          issue_prefetch(tmp_addr);
-        tmp_addr += BLOCK_SIZE;
-      }
-    }
+    //Addr tmp_addr = addr + BLOCK_SIZE;
+    //if (prev == -1) {
+    //  uint8_t strided = 0;
+    //  while (strided++ < STRIDED) {
+    //    if (!in_cache(tmp_addr) && !in_mshr_queue(tmp_addr))
+    //      issue_prefetch(tmp_addr);
+    //    tmp_addr += BLOCK_SIZE;
+    //  }
+    //}
     uint8_t depth = 0;
     uint8_t width = 0;
     Addr prev_addr;
